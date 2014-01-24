9a620f3dab62c5b91a8abdae5ba21e14e8d01c4c
kana <mao.akana@gmail.com>
2014-01-25 00:03:51 +0100

    Initial

---
+++ src/mediatheque/Mediatheque.java
@@ -0,0 +1,15 @@
+package mediatheque;
+
+/**
+ *
+ * @author kana
+ */
+public class Mediatheque {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
