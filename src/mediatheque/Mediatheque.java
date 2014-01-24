87b7f43fc6021b4a01f5369bc038a77d57312377
kana <mao.akana@gmail.com>
2014-01-25 00:05:00 +0100

    Hello world

--- src/mediatheque/Mediatheque.java
+++ src/mediatheque/Mediatheque.java
@@ -10,6 +10,6 @@ public class Mediatheque {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("hello mediatheque");
     }
 }
